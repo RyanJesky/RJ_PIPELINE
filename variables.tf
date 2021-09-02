f614ee00699ee05fa6d9d254f370372eb661ae45
RyanJesky <57146630+RyanJesky@users.noreply.github.com>
2021-09-02 14:16:18 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resourceGroupName" {
+    default = "MyFirstDeployment"
+    type = string
+    description = "Resource group name for the storage account to reside in"
+}
+
+variable "location" {
+    default = "eastus"
+    type = string
+    description = "Region that the storage account will reside in"
+}
